8ad85f379533b2bfcdb4a5364883eeed41336d81
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-24 19:24:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,9 @@ class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(hash)
+  def initialize(attributes)
+    attributes.each do |key,value|
+      
   end
 
 end
