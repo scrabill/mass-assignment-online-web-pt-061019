9aea7e3b75e41c7e4b02b523d825501265fe62f8
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-24 19:28:07 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes) # Given a hash...
+    attributes.each do |key, value| # Iterate over each key/value pair...
+      self.send(("#{key}="), value) # And create a method for each
+    end
+  end
+
 end
