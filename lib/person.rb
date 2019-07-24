72285f31504aa0631a1162548ef467c0768f1f67
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-24 19:27:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,14 +3,10 @@ class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value|
+  def initialize(attributes) # Given a hash...
+    attributes.each do |key, value| # Iterate over each key/value pair
       self.send(("#{key}="), value)
     end
-
-      # attributes.each {|key, value| self.send(("#{key}="), value)}
-
-
   end
 
 end
